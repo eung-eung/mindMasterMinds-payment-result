fa45cbe7ca0a51a1dfbcf4b65d4c59bc7a6bfe0d
eung-eung <boyvip5231@gmail.com>
2024-03-09 16:26:52 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
-
+const getParam = (key) => {
+  const urlParams = new URLSearchParams(window.location.search);
+  const myParam = urlParams.get(key);
+  return myParam;
+}
+const getPaymentResult = () => {
+  const responseCode = getParam('vnp_ResponseCode');
+  return responseCode === '00' ? true : false;
+}
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {/* <img
+        style={{ position: 'absolute', left: '50%', top: '50%', transform: 'translate(-50%,-50%)' }}
+        src='/images/download.png' /> */}
+      {getPaymentResult ? <img
+        style={{ position: 'absolute', left: '50%', top: '50%', transform: 'translate(-50%,-50%)' }}
+        src='/images/download.png' /> : <img
+        style={{ position: 'absolute', left: '50%', top: '50%', transform: 'translate(-50%,-50%)' }}
+        src='/images/failed.png' />}
+    </>
+
+
   );
 }
 
