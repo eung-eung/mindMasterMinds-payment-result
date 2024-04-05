996471d1bec42831cf8072f237d9a0643acc4c8f
thanh-dao <15thanh09@gmail.com>
2024-04-05 22:06:26 +0700

    fix: disable OutsideClick, disable EscapeKey, disable EnterKey

--- src/App.js
+++ src/App.js
@@ -1,53 +1,51 @@
-import { useEffect, useState } from 'react'
-import './App.css';
-import Swal from 'sweetalert2'
-import withReactContent from 'sweetalert2-react-content'
+import { useEffect, useState } from "react";
+import "./App.css";
+import Swal from "sweetalert2";
+import withReactContent from "sweetalert2-react-content";
 
 const getParam = (key) => {
-  const urlParams = new URLSearchParams(window.location.search);
-  const myParam = urlParams.get(key);
-  return myParam;
-}
+    const urlParams = new URLSearchParams(window.location.search);
+    const myParam = urlParams.get(key);
+    return myParam;
+};
 const getPaymentResult = () => {
-  const responseCode = getParam('vnp_ResponseCode');
+    const responseCode = getParam("vnp_ResponseCode");
 
-  return responseCode === '00' ? true : false;
-}
+    return responseCode === "00" ? true : false;
+};
 const showSwal = (status) => {
-  withReactContent(Swal).fire({
-    title: status ? "Payment complete!" : "Payment failed",
-    icon: status ? "success" : "error",
-
-  }).then(res => {
-
-    if (res.isConfirmed) {
-      console.log("aaa");
-      window.postMessage("aaaaa");
-    }
-  })
-}
+    withReactContent(Swal)
+        .fire({
+            title: status ? "Payment complete!" : "Payment failed",
+            icon: status ? "success" : "error",
+            allowOutsideClick: false,
+            allowEscapeKey: false,
+            allowEnterKey: false,
+        })
+        .then((res) => {
+            if (res.isConfirmed) {
+                window.postMessage("aaaaa");
+            }
+        });
+};
 function App() {
-  const MySwal = withReactContent(Swal)
-  const [result, setResult] = useState('loading')
-  useEffect(() => {
-    setTimeout(() => {
-      setResult(getPaymentResult())
-    }, 1000)
-  }, [])
-  return (
-    <div className='container'>
-      {result === 'loading' &&
-        <>
-          <span class="loader"></span>
-        </>
-      }
-      {
-        (result !== 'loading') && showSwal(result)
-      }
-    </div>
-
-
-  );
+    const MySwal = withReactContent(Swal);
+    const [result, setResult] = useState("loading");
+    useEffect(() => {
+        setTimeout(() => {
+            setResult(getPaymentResult());
+        }, 1000);
+    }, []);
+    return (
+        <div className="container">
+            {result === "loading" && (
+                <>
+                    <span className="loader"></span>
+                </>
+            )}
+            {result !== "loading" && showSwal(result)}
+        </div>
+    );
 }
 
 export default App;
